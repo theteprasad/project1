e9be3a22e900745ae3dadb58c7e07d3675d2c1d8
root <root@ip-172-31-13-138.ap-south-1.compute.internal>
2022-04-08 04:38:49 +0000

    project1

---
+++ API/api/api.py
@@ -0,0 +1,21 @@
+import time
+import datetime
+from flask import Flask
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/time')
+def get_current_time():
+    return {'time': time.time()}
+
